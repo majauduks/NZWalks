03a03c5202aa0724a3f4c62bd78764761ba28a59
majauduks <majauduks@gmail.com>
2023-10-31 14:51:56 +0100

    End of section two

---
+++ NZWalks/NZwalks.api/Data/NzWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.api.Models.Domain;
+
+namespace NZwalks.api.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+
+    }
+}
---
+++ NZWalks/NZwalks.api/Migrations/20231031124917_AddLatColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZwalks.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLatColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Lat",
+                table: "Regions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "Regions");
+        }
+    }
+}
